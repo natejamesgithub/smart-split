bee5abf7ed815c0aaec7773fdaee4410b583d991
natejaqua <nathanjamesaqua@gmail.com>
2025-07-03 15:56:20 -0500

    Fix: renamed AuthContext.js to AuthContext.jsx for Vite JSX support

--- client/src/pages/GroupList.jsx
+++ client/src/pages/GroupList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"; 
-import { useAuth } from "../context/AuthContext"; 
+import { useAuth } from "../context/AuthContext.jsx"; 
 import { Link } from "react-router-dom"; 
 import axios from "axios"; 
 
