e673a9d15dd0e1c06d3b0e428d003b915033f115
natejaqua <nathanjamesaqua@gmail.com>
2025-07-03 16:38:10 -0500

    forgot some useStates

--- client/src/pages/CreateGroup.jsx
+++ client/src/pages/CreateGroup.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react"; 
 import axios from "axios"; 
 
 const CreateGroup = () => {
